f3bc74f8213dd8642203cca4996d10e5ce823794
DinakiS <dinaki2017@yandex.ru>
2017-08-08 00:33:07 +0300

    Optimization & modal on click

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express'),
-      fs = require('fs');
+    fs = require('fs');
 let app = express(),
     config = require('./config');
 
@@ -7,61 +7,74 @@ app.get('/', (req, res) => {
     let css = fs.readFileSync('./css.css', 'utf8'),
         js = fs.readFileSync('./js.js', 'utf8'),
         tmpl = fs.readFileSync('./tmpl.html', 'utf8');
-    
+
     let page = parseInt(req.query.page) || 0;
-    
+
     if (!config.cacheFolder) {
         res.send('Write path to the folder in <b>config.json</b>');
     } else {
-        let files = getAllFiles(config.cacheFolder);
-        
-        
-        files.sort((a, b) => {
-            return b.stat['birthtime'] - a.stat['birthtime']
-        })
-        
         let html = '',
             offset = page * config.imgPerPage,
             limit = offset + config.imgPerPage;
-        
-        limit = limit < files.length ? limit : files.length;
-        
-        for (let i = offset; i < limit; i++) {
-            let file = files[i];
-            html += `<img src="data:image/jpeg;base64,${new Buffer(file.file).toString('base64')}" class='preview' data-path="${file.path}">`;
-        } 
-        
+
+        let files = getAllFiles(config.cacheFolder, { offset: offset, limit: limit });
+
+        files.forEach(file => {
+            html += `<div class="img-preview-wrap"><img src="data:image/jpeg;base64,${new Buffer(file.file).toString('base64')}" class='preview' alt="${file.path}"></div>`;
+        })
+
         /*files.forEach((file) => {
             html += `<img src="data:image/jpeg;base64,${new Buffer(file.file).toString('base64')}" class='preview' data-path="${file.path}">`;
         })*/
-        
+
         let replace = {
             css: css,
             js: js,
             content: html,
             files: files.length
         }
-        
+
         Object.keys(replace).forEach(key => tmpl = tmpl.replace(`{{${key}}}`, replace[key]));
         res.set('Content-Type', 'text/html');
         res.send(tmpl);
     }
 })
 
-function getAllFiles(dir) {
+function getAllFiles(dir, opt = { offset: 0, limit: config.imgPerPage }) {
     let result = [];
-    let list = fs.readdirSync(dir);
+    let statList = getAllStats(dir, opt);
     
-    list.forEach((file) => {
-        file = dir + '/' + file;
-        let stat = fs.statSync(file);
-        if (stat && stat.isDirectory()) 
-            result = result.concat(getAllFiles(file));
-        else 
-            result.push({ file: fs.readFileSync(file), stat: stat, path: file });
-    })
-    
+    opt.limit = opt.limit > statList.length ? statList.length : opt.limit;
+
+    for (let i = opt.offset; i < opt.limit; i++) {
+        result.push({
+            file: fs.readFileSync(statList[i].path),
+            stat: statList[i].stat,
+            path: statList[i].path
+        });
+    }
+
     return result;
+
+    function getAllStats(dir, opt) {
+        let list = fs.readdirSync(dir);
+        let res = [];
+        
+        list.forEach((file) => {
+            file = dir + '/' + file;
+            let stat = fs.statSync(file);
+            if (stat && stat.isDirectory())
+                res = res.concat(getAllStats(file));
+            else
+                res.push({ stat: stat, path: file });
+        })
+
+        res.sort((a, b) => {
+            return b.stat['birthtime'] - a.stat['birthtime']
+        })
+
+        return res;
+    }
 }
 
 app.listen(config.port, () => {
--- js.js
+++ js.js
@@ -1,28 +1,41 @@
 window.onload = () => {
+    let modal = document.getElementById('imgModal'),
+        modalImg = document.getElementById('modalImg'),
+        stat = document.getElementById('stat');
+
     let images = document.querySelectorAll('img.preview');
     images.forEach(img => {
-        img.onclick = (event) => { 
-            event.currentTarget.classList.toggle('preview')
+        img.onclick = (event) => {
+            let t = event.currentTarget;
+            modal.style.display = 'block';
+            modalImg.src = t.src;
+            stat.innerHTML = t.alt;
         }
     })
-    
+
+    modal.onclick = function (event) {
+        if (event.target.id == 'imgModal' || event.target.id == 'closeModal') {
+            modal.style.display = 'none';
+        }
+    }
+
     let buttons = document.querySelectorAll('.pages button');
     buttons.forEach(btn => {
         btn.onclick = event => {
             let target = event.currentTarget;
             let currPage = location.search.match(/page=(\d+)/);
             currPage = currPage ? parseInt(currPage[1]) : 0;
-            
+
             if (target.textContent == '<') {
                 currPage--;
             } else if (target.textContent == '>') {
                 currPage++;
             }
-            
+
             if (currPage < 0) currPage = 0;
-            
+
             location.search = 'page=' + currPage;
         }
     })
-}
 
+}
